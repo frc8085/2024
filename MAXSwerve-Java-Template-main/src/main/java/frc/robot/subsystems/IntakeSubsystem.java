3a77c101336eba32be509188d9f3dc5f4fb6af8b
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2024-01-20 12:09:00 -0500

    Add Break Beam to Intake Subsystem

--- MAXSwerve-Java-Template-main/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ MAXSwerve-Java-Template-main/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.IntakeConstants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -8,23 +12,30 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import static frc.robot.Constants.DriveConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  //imports motor id
+  // imports motor id
   private final CANSparkMax m_intakeMotor = new CANSparkMax(DriveConstants.kIntake1CanId, MotorType.kBrushless);
+  DigitalInput breakBeam = new DigitalInput(IntakeConstants.kIRPort);
+
   /** Creates a new ExampleSubsystem. */
-  public IntakeSubsystem() {}
+  public IntakeSubsystem() {
+
+  }
 
   /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   * An example method querying a boolean state of the subsystem (for example, a
+   * digital sensor).
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
 
-  public void forward(){
+  public void forward() {
     m_intakeMotor.set(1);
   }
-  public void stop(){
+
+  public void stop() {
     m_intakeMotor.set(0);
   }
+
   public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
@@ -33,10 +44,11 @@ public class IntakeSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    log();
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+  public void log() {
+    SmartDashboard.putBoolean("Break Beam", breakBeam.get());
   }
+
 }
